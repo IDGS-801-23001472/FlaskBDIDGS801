cf3bd157ab81d8b7da8a11e3aa394f938784f83f
IDGS-801-23001472 <82666@alumnos.utleon.edu.mx>
2026-02-16 18:47:51 -0600

    Insertar 16/02/2026

--- app.py
+++ app.py
@@ -18,11 +18,26 @@ def page_not_found(e):
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	create_form = forms.UserForm2(request.form)
+	#ORM select * from alumnos
+	alumno = Alumnos.query.all()
+	return render_template("index.html", form=create_form, alumno=alumno)
 
 @app.route("/alumnos",methods=['GET','POST'])
 def Alumnos():
+	create_form = forms.UserForm2(request.form)
+	if request.method=='POST':
+		alum = Alumnos(nombre = create_form.nombre.data,
+				 apaterno = create_form.apaterno.data,
+				 email = create_form.email.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for('index'))
 	return render_template("Alumnos.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	csrf.init_app(app)
+	db.init_app(app)
+	with app.app_context():
+		db.create_all()
+	app.run()
